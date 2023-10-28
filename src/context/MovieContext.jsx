38f13b88326a6dce3050a70b4f2f3ec8cb3c0d02
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-10-28 16:38:04 -0700

    added instructions

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -4,15 +4,11 @@ export const MovieContext = createContext();
 
 
 export function MovieProvider({ children }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  
 
-  useEffect(() => {
-    // console.log("searchTerm changed:", searchTerm);
-  }, [searchTerm]);
+  
 
   return (
-    <MovieContext.Provider value={{ searchTerm, setSearchTerm }}>
-      {children}
-    </MovieContext.Provider>
+   <></>
   );
 }
